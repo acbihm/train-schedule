a975dfb79aa67f0ae89f165f61e8a42265854948
Alex C. B <43941153+acbihm@users.noreply.github.com>
2019-04-13 20:54:41 -0500

    testing out connection further

--- app.js
+++ app.js
@@ -9,14 +9,3 @@ var config = {
 };
 firebase.initializeApp(config);
 
-var database = firebase.database();
-
-var connectionsRef = database.ref("/connections");
-var connectedRef = database.ref(".info/connected");
-connectedRef.on("value", function (snap) {
-
-    if (snap.val()) {
-        var con = connectionsRef.push(true);
-        con.onDisconnect().remove();
-    }
-});
